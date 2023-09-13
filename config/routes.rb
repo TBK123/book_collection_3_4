e88cd14aa39327c81be0834dccf12ddf2b0a81b9
TBK123 <lol383tarek@hotmail.com>
2023-09-12 23:03:06 -0500

    Added features and completed code

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root "books#index"
+  # Root route
+  root 'user_books#index'
 
+  # Resource routes
+  resources :user_books
+  resources :users
+  resources :books do
+
+  end
 end
